9e9a56ac34106494b833ac2a70e8ef7b98c96d68
mohammad <mohammadalirezaie081@gmail.com>
2026-07-16 20:43:04 +0330

    update blog list DTO

--- backend/src/services/blogs/blogListService.ts
+++ backend/src/services/blogs/blogListService.ts
@@ -2,8 +2,22 @@ import { prisma } from "../../config/db.ts"
 
 export const getBlogList = async () => {
     try {
-        const blogs = await prisma.blog.findMany()
-        return blogs
+        const blogs = await prisma.blog.findMany({
+            include: {
+                author: {
+                    select: {
+                        name: true,
+                        email: true,
+                    },
+                },
+            },
+        });
+
+        return blogs.map(({ author, ...blog }) => ({
+            ...blog,
+            author: author.name,
+            authorEmail: author.email,
+        }));
     } catch (error) {
         throw new Error("Failed to get blog list", { cause: error })
     }
